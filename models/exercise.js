c40a0517dfa5cdbf6962608af1795dc0ac73f7c1
Farouk <faroukkisuule55@gmail.com>
2021-05-08 14:43:58 -0400

    Added exercise model

--- models/exercise.js
+++ models/exercise.js
@@ -3,8 +3,37 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const exerciseSchema = new Schema({
-
-})
+        
+    day: Date,
+    totalDuration: {
+        type: Number
+    },
+    exercises: [{
+        type: {
+            type: String,
+            trim: true
+        },
+        name: {
+            type: String,
+            trim: true
+        },
+        duration: {
+            type: Number
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        distance: {
+            type: Number
+        }
+    }]
+});
 
 const Exercise = mongoose.model("Exercise", exerciseSchema);
 
